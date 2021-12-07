3b324c72d6e5fe1e36a2f2b5b6ca0bfc177c5f6e
Pierre-Yves Martin <pym.aldebaran@gmail.com>
2021-12-07 10:18:08 +0100

    Solve day 1 part 1

--- advent.py
+++ advent.py
@@ -18,8 +18,10 @@ in the Advent calendar; the second puzzle is unlocked when you complete the
 first. Each puzzle grants one star. Good luck!
 """
 
-from typing import List
+from typing import List, Tuple
 from itertools import pairwise
+from dataclasses import dataclass
+
 from more_itertools import windowed
 
 
@@ -129,12 +131,95 @@ def day1_bis(all_depth: List[int]) -> int:
     return day1(sum_depth)
 
 
+class UnknownCommand(Exception):
+    def __init__(self, command: str):
+        self.command = command
+        super().__init__(f"Unknown command: {command}")
+
+
+@dataclass
+class Command:
+    delta_h: int
+    delta_d: int
+
+    def __init__(self, com: str, val: int):
+        match com:
+            case "forward":
+                self.delta_h = val
+                self.delta_d = 0
+            case "down":
+                self.delta_h = 0
+                self.delta_d = val
+            case "up":
+                self.delta_h = 0
+                self.delta_d = -val
+            case _:
+                raise UnknownCommand(com)
+
+
+def day2(commands: List[Command]) -> (int, int):
+    """
+    --- Day 2: Dive! ---
+
+    Now, you need to figure out how to pilot this thing.
+
+    It seems like the submarine can take a series of commands like forward 1,
+    down 2, or up 3:
+
+        forward X increases the horizontal position by X units.
+        down X increases the depth by X units.
+        up X decreases the depth by X units.
+
+    Note that since you're on a submarine, down and up affect your depth, and so
+    they have the opposite result of what you might expect.
+
+    The submarine seems to already have a planned course (your puzzle input).
+    You should probably figure out where it's going. For example:
+
+    forward 5
+    down 5
+    forward 8
+    up 3
+    down 8
+    forward 2
+
+    Your horizontal position and depth both start at 0. The steps above would
+    then modify them as follows:
+
+        forward 5 adds 5 to your horizontal position, a total of 5.
+        down 5 adds 5 to your depth, resulting in a value of 5.
+        forward 8 adds 8 to your horizontal position, a total of 13.
+        up 3 decreases your depth by 3, resulting in a value of 2.
+        down 8 adds 8 to your depth, resulting in a value of 10.
+        forward 2 adds 2 to your horizontal position, a total of 15.
+
+    After following these instructions, you would have a horizontal position of
+    15 and a depth of 10. (Multiplying these together produces 150.)
+
+    Calculate the horizontal position and depth you would have after following
+    the planned course. What do you get if you multiply your final horizontal
+    position by your final depth?
+    """
+    delta_h = [com.delta_h for com in commands]
+    delta_d = [com.delta_d for com in commands]
+    return sum(delta_h), sum(delta_d)
+
+
 def main():
     with open("input_day1.txt") as all_depth:
         all_depth_int = [int(depth) for depth in all_depth if depth != "\n"]
 
     print("Day 01    :", day1(all_depth_int))
     print("Day 01 bis:", day1_bis(all_depth_int))
+
+    with open("input_day2.txt") as instructions_file:
+        instructions = [
+            (Command(com, int(val)))
+            for com, val in (line.split() for line in instructions_file if line != "\n")
+        ]
+
+    h_pos, d_pos = day2(instructions)
+    print("Day 02    :", h_pos * d_pos)
 
 
 if __name__ == "__main__":
--- test_advent.py
+++ test_advent.py
@@ -1,20 +1,25 @@
-from advent import day1, day1_bis
+from advent import day1, day1_bis, Command, day2
 
-SIMPLE_DAY_1 = [
-        199,
-        200,
-        208,
-        210,
-        200,
-        207,
-        240,
-        269,
-        260,
-        263
-    ]
+SIMPLE_DAY_1 = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
 
 def test_day1_on_simple_exemple():
     assert day1(SIMPLE_DAY_1) == 7
 
+
 def test_day1bis_on_simple_exemple():
     assert day1_bis(SIMPLE_DAY_1) == 5
+
+
+SIMPLE_DAY_2 = [
+    Command("forward", 5),
+    Command("down", 5),
+    Command("forward", 8),
+    Command("up", 3),
+    Command("down", 8),
+    Command("forward", 2),
+]
+
+
+def test_day2_on_simple_exemple():
+    assert day2(SIMPLE_DAY_2) == (15, 10)
